7887a89e9c9aa6141f0710f8be9f51a135efe3eb
Dominic Vogl <dv@bergwerk.ag>
2015-04-17 18:35:09 +0200

    [task] update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,55 +2,47 @@
 // Gulp Configuration
 //------------------------------------------------------------------------
 
-
 //
 // Include necessary gulp files
 // ------------------------------------------------------------------------
 
 var gulp =
-   require('gulp'),
+      require('gulp'),
 // path = require('path'),
    minifyCSS = require('gulp-minify-css'),
    less = require('gulp-less'),
    concat = require('gulp-concat'),
 // uglify = require('gulp-uglify'),
    plumber = require('gulp-plumber'),
-   rename = require("gulp-rename");
+   rename = require("gulp-rename"),
 // sourcemaps = require("gulp-sourcemaps");
 // autoprefixer = require("gulp-autoprefixer");
    watch = require('gulp-watch');
 
-
-var onError = function(event) {
-   return gulp.src(event.path)
-      .pipe(refresh(lrserver));
-};
-
+// Define Path Variables
 var themepath = 'wp-content/themes/bwrk_devkit';
 
 //
-// Compile SASS files
+// Compile LESS files
 //------------------------------------------------------------------------
 
 gulp.task('styles', function () {
 
-   gulp.src(themepath+'/src/less/main.less')
+   gulp.src(themepath + '/src/less/main.less')
       .pipe(plumber())
       .pipe(less())
       .pipe(rename({basename: 'app', extname: '.css'}))
-      .pipe(gulp.dest(themepath+'/css/'))
+      .pipe(gulp.dest(themepath + '/css/'))
       .pipe(minifyCSS())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(themepath+'/css/'))
+      .pipe(gulp.dest(themepath + '/css/'))
 });
-
-
 
 //
 // Concatenate & Minify JS
 // ------------------------------------------------------------------------
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
 
    var filelist = [
 
@@ -62,24 +54,31 @@ gulp.task('scripts', function() {
       .pipe(plumber())
       .pipe(concat('main.js'))
       //.pipe(uglify('compress'))
-      .pipe(gulp.dest(themepath+'/js/'))
+      .pipe(gulp.dest(themepath + '/js/'))
 });
-
-
 
 //
 // Watch files for changes
 // ------------------------------------------------------------------------
 
-gulp.task('watch', function() {
-   gulp.watch(themepath+'/src/less/**/*.less', ['styles'], onError);
-   gulp.watch(themepath+'/src/js/**/*.js', ['scripts'], onError);
+gulp.task('watch', function () {
+   gulp.watch(themepath + '/src/less/**/*.less', ['styles'], errorHandling);
+   gulp.watch(themepath + '/src/js/**/*.js', ['scripts'], errorHandling);
 });
-
-
 
 //
 // Define default Tasks
 //------------------------------------------------------------------------
 
-gulp.task('default', ['styles', 'scripts', 'watch'])
+gulp.task('default', ['styles', 'scripts', 'watch']);
+
+/**
+ * Error handler function
+ * @param event
+ * @returns {*}
+ */
+
+function errorHandling(event) {
+   return gulp.src(event.path)
+      .pipe(refresh(lrserver));
+}
