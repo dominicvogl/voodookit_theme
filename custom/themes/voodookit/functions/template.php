9caa8acacf737f21125f3bd894f4b49e428d6ea6
Dominic Vogl <dv@bergwerk.ag>
2015-05-15 14:55:38 +0200

    [task] add basic template functions

--- custom/themes/voodookit/functions/template.php
+++ custom/themes/voodookit/functions/template.php
@@ -1,100 +1,43 @@
 <?php
 
-$f5_config = array();
-
-if(function_exists('get_field')) {
-    $f5_config['gmaps_api_key'] .= get_field('gmaps_api_key', 'options');
-}
-
-
-
 //
-// Favicons laden
+// Load favicons
 // ----------------------------------------------------------------------------------------
 
-function blog_favicon() {
+function add_favicons()
+{
     $url = get_bloginfo('template_url');
     echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.$url.'/img/favicon.ico" />';
     echo '<link rel="Shortcut Icon" type="image/png" href="'.$url.'/img/favicon.png" />';
 }
-add_action('wp_head', 'blog_favicon');
+
+add_action('wp_head', 'add_favicons');
 
 
 
 //
-// Get Template dir
-// --------------------------------------------------------------------
-
-function get_dir($path) {
-
-	return get_template_directory_uri().$path;
-
-}
-
-
-
-//
-// Lade CSS
+// Load CSS files
 // ----------------------------------------------------------------------------------------
 
-function load_css() {
+function load_css()
+{
 
 	if(!is_admin()) {
 
-		$styles = array(
-
-			array(
-				'handle'	=> 'gfonts',
-				'src'		=> 'http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto:400,500',
-				'deps'		=> array(),
-			),
+		$files = array(
 
 			array(
 				'handle'	=> 'styles',
-				'src'		=> get_dir('/styles/styles.css'),
-				'deps'		=> array(),
+				'src'		=> get_template_directory_uri() . '/css/app.css',
+            'deps'	=> array(),
 			)
 
 		);
 
-		if( is_page( array(1433, 1556)) ) {
-			$styles = array(
+		foreach( $files as $file ) {
 
-				array(
-					'handle'	=> 'gfonts',
-					'src'		=> 'http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto:400,500',
-					'deps'		=> array(),
-				),
-
-				array(
-					'handle'	=> 'styles',
-					'src'		=> get_dir('/styles/styles.css'),
-					'deps'		=> array(),
-				),
-
-				array(
-					'handle'	=> 'schock',
-					'src'		=> get_dir('/styles/schock.css'),
-					'deps'		=> array(),
-				)
-			);
-
-		}
-
-		if( is_page(1556) ) {
-
-			$styles[] = array(
-				'handle'	=> 'video',
-				'src'		=> get_dir('/styles/video-js.min.css'),
-				'deps'		=> array(),
-			);
-
-		}
-
-		foreach( $styles as $style ) {
-
-			wp_register_style( $style['handle'], $style['src'], $style['deps'], '2.0' );
-			wp_enqueue_style( $style['handle'] );
+			wp_register_style( $file['handle'], $file['src'], $file['deps'], BWRK_TEMPLATE_VERSION );
+			wp_enqueue_style( $file['handle'] );
 
 		}
 
@@ -110,7 +53,8 @@ add_action('wp_enqueue_scripts', 'load_css');
 // Remove standard wordpress jquery library
 // ----------------------------------------------------------------------------------------
 
-function modify_jquery() {
+function modify_jquery()
+{
 	if (!is_admin()) {
 		wp_deregister_script('jquery');
 	}
@@ -124,84 +68,25 @@ add_action('init', 'modify_jquery');
 // Lade Javascript
 // ----------------------------------------------------------------------------------------
 
-function load_javascript() {
+function load_javascript()
+{
 
 	if(!is_admin()) {
 
-		$scripts = array(
+		$files = array(
 
 			array(
-				'handle'	=> 'modernizr',
-				'src'		=> get_dir( '/js/cccc/modernizr.js' ),
-				'deps'		=> array(),
-			),
-
-			array(
-				'handle'	=> 'jquery',
-				'src'		=> 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
-				'deps'		=> array('modernizr'),
-			),
-
-			array(
-				'handle'	=> 'fastclick',
-				'src'		=> get_dir( '/js/cccc/fastclick.js' ),
-				'deps'		=> array('jquery'),
-			),
-
-			array(
-				'handle'	=> 'ssm',
-				'src'		=> get_dir( '/js/cccc/ssm.min.js' ),
-				'deps'		=> array('jquery'),
-			),
-
-			array(
-				'handle'	=> 'jpanelmenu',
-				'src'		=> get_dir( '/js/cccc/jquery.jpanelmenu.min.js' ),
-				'deps'		=> array('jquery'),
-			),
-
-			array(
-				'handle'	=> 'slick',
-				'src'		=> get_dir( '/js/cccc/slick.min.js' ),
-				'deps'		=> array('jquery'),
-			),
-
-			array(
-				'handle'	=> 'strip',
-				'src'		=> get_dir( '/js/cccc/strip.min.js' ),
-				'deps'		=> array('jquery'),
-			),
+				'handle'	=> 'app',
+				'src'		=> get_template_directory_uri() .'/js/app.js',
+				'deps'	=> array(),
+			)
 
 		);
 
-		if(is_post_type('gallery')) {
-			$scripts[] = array(
-				'handle'	=> 'wookmark',
-				'src'		=> get_dir( '/js/cccc/jquery.wookmark.min.js' ),
-				'deps'		=> array('jquery'),
-			);
-		}
+		foreach( $files as $file ) {
 
-		if( is_page(1556) ) {
-
-			$scripts[] = array(
-				'handle'	=> 'video',
-				'src'		=> get_dir( '/js/cccc/video.min.js' ),
-				'deps'		=> array('jquery'),
-			);
-
-		}
-
-		$scripts[] = array(
-			'handle'	=> 'setup',
-			'src'		=> get_dir( '/js/cccc/setup.js' ),
-			'deps'		=> array('jquery'),
-		);
-
-		foreach( $scripts as $script ) {
-
-			wp_register_script( $script['handle'], $script['src'], $script['deps'], '2.0' );
-			wp_enqueue_script( $script['handle'] );
+			wp_register_script( $file['handle'], $file['src'], $file['deps'], BWRK_TEMPLATE_VERSION );
+			wp_enqueue_script( $file['handle'] );
 
 		}
 
@@ -217,7 +102,8 @@ add_action('wp_footer', 'load_javascript');
 // Header aufräumen
 // ----------------------------------------------------------------------------------------
 
-function remheadlink() {
+function remheadlink()
+{
     // Remove links to the extra feeds (e.g. category feeds)
     remove_action( 'wp_head', 'feed_links_extra', 3 );
     // Remove links to the general feeds (e.g. posts and comments)
@@ -239,21 +125,3 @@ function remheadlink() {
 }
 
 add_action('init', 'remheadlink');
-
-
-
-//
-// Shotcodes
-// ----------------------------------------------------------------------------------------
-
-function vimeo_short($video){
-	//echo '<h3>'.$video['title'].'</h3>';
-	echo '<div class="video-box"><iframe src="//player.vimeo.com/video/'.$video['id'].'?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
-}
-add_shortcode( 'vimeo', 'vimeo_short' );
-
-function youtube_short($video){
-	//echo '<h3>'.$video['title'].'</h3>';
-	echo '<div class="video-box"><iframe width="560" height="315" src="//www.youtube.com/embed/'.$video['id'].'" frameborder="0" allowfullscreen></iframe></div>';
-}
-add_shortcode( 'youtube', 'youtube_short' );
