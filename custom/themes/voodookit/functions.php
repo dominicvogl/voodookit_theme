b08a133531d82c775b81a79c3eba55b533dadbb3
Dominic Vogl <dv@bergwerk.ag>
2015-05-18 11:13:25 +0200

    [task] move template functions into bwrk_monsterkill extension

--- custom/themes/voodookit/functions.php
+++ custom/themes/voodookit/functions.php
@@ -1,22 +1,12 @@
 <?php
 
-//
-// Import Template Files
-// ------------------------------------------------------------------------------
-
 // List all file to import
 $files = array(
-   'helpers',
-   'admin',
-   'template',
-   'acf-blocks',
-   'images',
-   'items',
-   'loops'
+   'template'
 );
 
 if (is_array($files)) {
    foreach ($files as $filename) {
-      include(TEMPLATEPATH . '/functions/' . $filename . '.php');
+      include(plugin_dir_path( __FILE__ ) . 'includes/' . $filename . '.php');
    }
 }
