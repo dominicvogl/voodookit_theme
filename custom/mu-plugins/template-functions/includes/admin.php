fada4558e81157e2dddbb81da0711df78c579908
Dominic Vogl <dv@cat-ia.de>
2015-09-05 13:43:25 +0200

    [task] change acf option page activation

--- custom/mu-plugins/template-functions/includes/admin.php
+++ custom/mu-plugins/template-functions/includes/admin.php
@@ -4,17 +4,35 @@
 // Register ACF options page
 // ----------------------------------------------------------------------------------------
 
-if( function_exists('acf_add_options_page') ) {
+function add_acf_option_pages()
+{
 
-    $page = acf_add_options_page(array(
-        'page_title'    => 'Theme General Settings',
-        'menu_title'    => 'Theme Settings',
-        'menu_slug'     => 'theme-general-settings',
-        'capability'    => 'edit_posts',
-        'redirect'      => false
-    ));
+    if( function_exists('acf_add_options_page') ) {
 
+        acf_add_options_page(array(
+           'page_title' 	=> 'Theme General Settings',
+           'menu_title'	=> 'Theme Settings',
+           'menu_slug' 	=> 'theme-general-settings',
+           'capability'	=> 'edit_posts',
+           'redirect'		=> false
+        ));
+
+        acf_add_options_sub_page(array(
+           'page_title' 	=> 'Theme Header Settings',
+           'menu_title'	=> 'Header',
+           'parent_slug'	=> 'theme-general-settings',
+        ));
+
+        acf_add_options_sub_page(array(
+           'page_title' 	=> 'Theme Footer Settings',
+           'menu_title'	=> 'Footer',
+           'parent_slug'	=> 'theme-general-settings',
+        ));
+
+    }
 }
+
+add_action('admin_menu', 'add_acf_option_pages');
 
 
 
