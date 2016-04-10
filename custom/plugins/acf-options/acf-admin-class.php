29268382fcac6498f3507169c4d1dc76959db83e
Dominic <dv@cat-ia.de>
2016-04-10 15:14:20 +0200

    update acf plugin, for basic config and acf json group saves

--- custom/plugins/acf-options/acf-admin-class.php
+++ custom/plugins/acf-options/acf-admin-class.php
@@ -8,14 +8,16 @@ class Voo_ACF_Admin
    public function __construct()
    {
 
+      // add ACF Option Page
       add_action('admin_menu', array($this, 'add_acf_option_pages'));
 
+      // add ACF JSON save point
+      add_filter('acf/settings/save_json', array($this, 'my_acf_json_save_point'));
+
+      // add ACF JSON load point
+      add_filter('acf/settings/load_json', array($this, 'my_acf_json_load_point'));
 
    }
-
-   //
-   // Register ACF options page
-   // ----------------------------------------------------------------------------------------
 
    public function add_acf_option_pages()
    {
@@ -31,8 +33,8 @@ class Voo_ACF_Admin
          ));
 
          acf_add_options_sub_page(array(
-            'page_title' => 'Theme Header Settings',
-            'menu_title' => 'Header',
+            'page_title' => 'archive-mitglied',
+            'menu_title' => 'Archiv: Mitglieder',
             'parent_slug' => 'theme-general-settings',
          ));
 
@@ -43,6 +45,35 @@ class Voo_ACF_Admin
          ));
 
       }
+   }
+
+
+   public function my_acf_json_save_point($path)
+   {
+
+      // update path
+      $path = plugin_dir_path( __FILE__ ) . '/acf-json';
+
+
+      // return
+      return $path;
+
+   }
+
+   public function my_acf_json_load_point($paths)
+   {
+
+      // remove original path (optional)
+      unset($paths[0]);
+
+
+      // append path
+      $paths[] = plugin_dir_path( __FILE__ ) . '/acf-json';
+
+
+      // return
+      return $paths;
+
    }
 
 }
