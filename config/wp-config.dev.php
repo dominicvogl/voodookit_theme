70fdb76bdd5459dc96129a5c21d69f782c93e571
Dominic Vogl <dominic.vogl@sigikid.de>
2017-03-07 16:31:17 +0100

    init project

---
+++ config/wp-config.dev.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @var $hostname
+ */
+  
+
+// ** MySQL settings - You can get this info from your web host ** //
+define( 'DB_NAME', 'voodookit' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+define('WP_HOME','http://wp-voodookit.local/');
+define('WP_SITEURL','http://wp-voodookit.local/core');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+
+// Schaltet den Debug Mode ein. Dadurch sind Fehler im Frontend sichtbar.
+define( 'WP_DEBUG', true );
+
+// Verhindert, dass Fehlermeldungen im Frontend sichtbar sind.
+define( 'WP_DEBUG_DISPLAY', false );
+
+// Zeige verschiedene Fehler an
+// E_ALL
+// E_ERROR | E_WARNING | E_PARSE | E_NOTICE
+@ini_set( 'display_errors', 0 );
+
+// Aktiviert das Logfile und legt es im Verzeichnis /wp-content/debug.log ab.
+define( 'WP_DEBUG_LOG', true );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', false );
--- config/wp-config.env.php
+++ config/wp-config.env.php
@@ -1,29 +1,15 @@
 <?php
 /**
- * Setup environments
- * 
- * Set environment based on the current server hostname, this is stored
- * in the $hostname variable
- * 
- * You can define the current environment via: 
- *     define('WP_ENV', 'production');
- * 
- * @package    Studio 24 WordPress Multi-Environment Config
- * @version    1.0
- * @author     Studio 24 Ltd  <info@studio24.net>
+ * @var $hostname
  */
 
 
 // Set environment based on hostname
 switch ($hostname) {
-    case 'clients.local':
+    case 'wp-voodookit.local':
         define('WP_ENV', 'dev');
         break;
-    
-    case 'www.domain.com':
-        define('WP_ENV', 'stage');
-        break;
-    
+
     default: 
         define('WP_ENV', 'prod');
 }
