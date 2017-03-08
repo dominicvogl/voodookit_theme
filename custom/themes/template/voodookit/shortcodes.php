4038a132438e8b720d0842c8408b69b8a1ef8e63
Dominic Vogl <dominic.vogl@sigikid.de>
2017-03-08 16:32:57 +0100

    shortcode bugfix

--- custom/themes/template/voodookit/shortcodes.php
+++ custom/themes/template/voodookit/shortcodes.php
@@ -2,21 +2,17 @@
 
 if(function_exists('add_shortcode')) {
 
-	add_shortcode( 'vimeo', array($this, 'vimeo_short') );
-	add_shortcode( 'youtube', array($this, 'youtube_short') );
+	add_shortcode( 'vimeo', 'vimeo_short' );
+	add_shortcode( 'youtube', 'youtube_short');
 
 }
 
 function vimeo_short($video)
 {
-	$html = '<div class="video-box"><iframe src="//player.vimeo.com/video/'.$video['id'].'?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
-
-	echo $html;
+	echo '<div class="video-box"><iframe src="//player.vimeo.com/video/'.$video['id'].'?title=0&amp;byline=0&amp;portrait=0&amp;" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
 }
 
 function youtube_short($video)
 {
-	$html = '<div class="video-box"><iframe width="560" height="315" src="//www.youtube.com/embed/'.$video['id'].'" frameborder="0" allowfullscreen></iframe></div>';
-
-	echo $html;
+	echo '<div class="video-box"><iframe width="560" height="315" src="//www.youtube.com/embed/'.$video['id'].'" frameborder="0" allowfullscreen></iframe></div>';
 }
