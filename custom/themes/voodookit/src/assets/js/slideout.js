c257c7840ef3c1ccf04c77bd18bf0c80a76e4857
Dominic Vogl <dominic.vogl@sigikid.de>
2019-08-14 20:38:58 +0200

    add slideout animation style, close area

--- custom/themes/voodookit/src/assets/js/slideout.js
+++ custom/themes/voodookit/src/assets/js/slideout.js
@@ -2,13 +2,31 @@ import { MediaQuery } from 'foundation-sites';
 import Slideout from 'slideout';
 
 let toggler = document.querySelector('.js-toggle-slideout');
-
-const slideout = new Slideout({
+let slideout;
+slideout = new Slideout({
 	'panel': document.querySelector('.js-slideout-panel'),
 	'menu': document.querySelector('.js-slideout-menu'),
 	'padding': 256,
 	'tolerance': 70
 });
+
+let close;
+close = (eve) => {
+	eve.preventDefault();
+	slideout.close();
+};
+
+slideout
+	.on('beforeopen', function() {
+		this.panel.classList.add('panel-open');
+	})
+	.on('open', function() {
+		this.panel.addEventListener('click', close);
+	})
+	.on('beforeclose', function() {
+		this.panel.classList.remove('panel-open');
+		this.panel.removeEventListener('click', close);
+	});
 
 const setBodyPadding = ( togglerHeight ) => {
 	document.querySelector('body').style.paddingBottom = togglerHeight + 'px';
