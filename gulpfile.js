aec53a1ec813e70439d85153f7a162b6bdf56238
Dominic Vogl <dominic.vogl@sigikid.de>
2019-08-16 19:22:46 +0200

    add svg sprite rendering via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,129 +1,140 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    inject = require('gulp-inject'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    plumber = require('gulp-plumber'),
-    babel = require('gulp-babel'),
-    browserify = require('gulp-browserify'),
-    clean = require('gulp-clean'),
-    sourcemaps = require('gulp-sourcemaps'),
-    htmlmin = require('gulp-html-minifier'),
-    iconfont = require('gulp-iconfont'),
-    consolidate = require('gulp-consolidate'),
-    path = require('path'),
-    browserSync = require('browser-sync');
+/**
+ * @function require
+ * @type {Gulp}
+ */
+
+const 	gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	cleanCSS = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	plumber = require('gulp-plumber'),
+	babel = require('gulp-babel'),
+	browserify = require('gulp-browserify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	svgSprite = require('gulp-svg-sprite'),
+	browserSync = require('browser-sync');
+
+const 	src = './src/',
+	dist = './dist/';
 
 
-const src = './src/',
-    dist = './dist/';
-
-
-// ################################
-// Minify SASS
+/**
+ * Compile and compress sass files into css file
+ * @since 1.0.0
+ */
 
 const gulpScss = () => {
-    gulp.src(src + 'assets/scss/*.scss')
-        .pipe(sourcemaps.init())
-        // .pipe(clean())
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer())
-        // .pipe(rename({basename: 'app'}))
-        // .pipe(gulp.dest(dist + 'assets/css'))
-        .pipe(cleanCSS())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dist + 'assets/css'))
-        .pipe(browserSync.stream());
+	gulp.src(src + 'assets/scss/*.scss')
+		.pipe(sourcemaps.init())
+		// .pipe(clean())
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer())
+		// .pipe(rename({basename: 'app'}))
+		// .pipe(gulp.dest(dist + 'assets/css'))
+		.pipe(cleanCSS())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(dist + 'assets/css'))
+		.pipe(browserSync.stream());
 };
 
 
-// ################################
-// Minify JS
+/**
+ * Concat javascript files and compress them
+ * @since 1.0.0
+ */
 
-const gulpJS = () => {
-    gulp.src(src + 'assets/js/*.js')
-        .pipe(sourcemaps.init()) // start sourcemap
-        .pipe(plumber()) // prevent gulp crash on error event
-        .pipe(concat('app.js')) // define filename after merging all files
-        .pipe(babel({
-            presets: ['es2015']
-        })) // Use ES6 or ES7 and compile to "normal" javascript for browsercompatibility
-        .pipe(browserify({
-            insertGlobal: true,
-            debug: !gulp.env.production
-        })) // Use fileimports from node modules
-        .pipe(uglify()) // minify javascript
-        .pipe(rename({suffix: '.min'})) // add sufficx
-        .pipe(sourcemaps.write('.')) // write sourcemap
-        .pipe(gulp.dest(dist + 'assets/js')) // define destination folder
-        .pipe(browserSync.stream());
+const gulpJs = () => {
+	gulp.src(src + 'assets/js/*.js')
+		.pipe(sourcemaps.init()) // start sourcemap
+		.pipe(plumber()) // prevent gulp crash on error event
+		.pipe(concat('app.js')) // define filename after merging all files
+		.pipe(babel({
+			presets: ['es2015']
+		})) // Use ES6 or ES7 and compile to "normal" javascript for browsercompatibility
+		.pipe(browserify({
+			insertGlobal: true
+		})) // Use fileimports from node modules
+		.pipe(uglify()) // minify javascript
+		.pipe(rename({suffix: '.min'})) // add sufficx
+		.pipe(sourcemaps.write('.')) // write sourcemap
+		.pipe(gulp.dest(dist + 'assets/js')) // define destination folder
+		.pipe(browserSync.stream());
 };
 
 
-// ################################
-// Minify HTML
 
-const gulpHTML = () => {
-    gulp.src(dist + '*.html', {force: true})
-        .pipe(clean());
-    gulp.src(src + '*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest(dist))
-        .pipe(browserSync.stream());
+/**
+ * Gulp generate SVG Sprite
+ */
+
+const gulpSvg = () => {
+
+	gulp.src('**/*.svg', { cwd: src + 'assets/svg/' })
+		.pipe(plumber())
+		.pipe(svgSprite(
+			{
+				mode: {
+					'symbol': {
+						'dest': './assets/svg',
+						'sprite': 'sprite-symbol'
+					}
+				}
+			}
+		))
+		.on('error', function(error) {
+			/* Do some awesome error handling ... */
+		})
+		.pipe(gulp.dest(dist));
+
+};
+
+/**
+ * Watch some files and their changes
+ */
+
+const gulpWatch = () => {
+
+	// watch some files
+	gulp.watch([src + 'assets/scss/**/*.scss'], ['scss']);
+	gulp.watch([src + 'assets/js/*.js'], ['js']);
+	gulp.watch([src + 'assets/svg/*.svg'], ['svg']);
+
 };
 
 
-gulp.task('icons', iconsTask);
+/**
+ * Default gulp task
+ */
 
-function iconsTask() {
-    gulp.src([sourcePath + 'assets/svg/use/*.svg'])
-        .pipe(iconfont({
-            fontName: 'icon',
-            appendCodepoints: true
-        }))
-        .on('codepoints', function (codepoints, options) {
-            gulp.src(sourcePath + 'scss/templates/_icons.scss')
-                .pipe(consolidate('lodash', {
-                    glyphs: codepoints,
-                    fontName: 'icon',
-                    fontPath: '../fonts/generated/',
-                    className: 'icon'
-                }))
-                .pipe(gulp.dest(sourcePath + 'scss/generated'));
-        })
-        .pipe(gulp.dest(destinationPath + 'fonts/generated'));
-}
+const gulpDefault = () => {
+
+	// Initial compiling of files
+	gulpScss();
+	gulpJs();
+	gulpSvg();
+	// gulpHTML();
+
+	// Start browsersync server
+	// browserSync.init({
+	//     server: './dist' // define folder to watch
+	// }); // start server for effective developing
+
+	gulpWatch();
+};
 
 
-// ################################
-// define gulp tasks
+/**
+ * Define all the gulp tasks
+ */
 
 gulp.task('scss', gulpScss);
-gulp.task('js', gulpJS);
+gulp.task('js', gulpJs);
 // gulp.task('html', gulpHTML);
-
-
-// ################################
-// Watcher
-
-gulp.task('default', function () {
-    // Initial compiling of files
-    gulpScss();
-    gulpJS();
-    // gulpHTML();
-
-    // Start browsersync server
-    // browserSync.init({
-    //     server: './dist' // define folder to watch
-    // }); // start server for effective developing
-
-    // watch some files
-    // gulp.watch([src + '*.html'], ['html']);
-	gulp.watch([src + 'assets/scss/**/*.scss'], ['scss']);
-    gulp.watch([src + 'assets/js/*.js'], ['js']);
-});
+gulp.task('svg', gulpSvg);
+gulp.task('watch', gulpWatch());
+gulp.task('default', gulpDefault());
