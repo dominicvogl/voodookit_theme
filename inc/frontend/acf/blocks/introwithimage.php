6ab435505967c25fe019c25c2cd9c1186b71f747
Dominic Vogl <dominic.vogl@sigikid.de>
2019-09-18 19:13:07 +0200

    add more features to the intro element

--- inc/frontend/acf/blocks/introwithimage.php
+++ inc/frontend/acf/blocks/introwithimage.php
@@ -11,7 +11,9 @@ $fields = [
 	'title' => get_field('title'),
 	'content' => get_field('content'),
 	'image' => get_field('image'),
-	'call_to_action' => get_field('call_to_action')
+	'call_to_action' => get_field('call_to_action'),
+	'bg_icon' => get_field('bg_icon'),
+	'bg_color' => get_field('bg_color'),
 ];
 
 if(! function_exists('voodookit_acf_block_get_btn_label')) {
@@ -40,10 +42,28 @@ $block_id = $block_classes[0] . '-' . $block['id'];
 
 $block_classes = trim(implode(' ', $block_classes), ' ');
 
+$blockstyles = [];
+if($fields['image']) {
+	$blockstyles[] = 'background-image: url("'.esc_url($fields['image']['url']).'");';
+}
+if($fields['bg_color']) {
+	$blockstyles[] = 'background-color: '.$fields['bg_color'].';';
+}
+
+$blockstyles = esc_attr( implode( ' ', $blockstyles ) );
+
 ?>
-<section id="<?php echo $block_id; ?>" class="<?php echo $block_classes; ?>" style="background-image: url('<?php echo esc_url($fields['image']['url']); ?>')">
+<section id="<?php echo $block_id; ?>" class="<?php echo $block_classes; ?>" style="<?php echo $blockstyles; ?>">
 
 	<?php
+
+	if ( $fields['bg_icon'] ) {
+		echo
+			'<div class="intro-block-icon">
+			' . wp_get_attachment_image( $fields['bg_icon']['id'] ) . '
+		</div>';
+	}
+
 	if(is_array($fields)) {
 
 		echo '<div class="row"><div class="column">';
