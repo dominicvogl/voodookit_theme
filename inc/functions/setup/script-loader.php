3619950d4a5c6356d8a155270272aa1ad55ca893
Dominic Vogl <dominic.vogl@sigikid.de>
2019-11-13 13:34:23 +0100

    remove script querys

--- inc/functions/setup/script-loader.php
+++ inc/functions/setup/script-loader.php
@@ -6,6 +6,15 @@
  * @package Voodookit
  */
 
+function voodookit_remove_wp_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+
+add_filter( 'style_loader_src', 'voodookit_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'voodookit_remove_wp_ver_css_js', 9999 );
+
 /**
  * Load CSS Files
  *
