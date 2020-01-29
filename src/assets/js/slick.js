39a0f274528acfd6687aa6eacf733415d00e3be4
Dominic Vogl <dominic.vogl@sigikid.de>
2020-01-29 15:20:03 +0100

    default slider module, element

--- src/assets/js/slick.js
+++ src/assets/js/slick.js
@@ -2,8 +2,22 @@ import $ from 'jquery'
 import 'slick-slider'
 
 $('.js-slick-slider').each(function() {
-	$(this).slick({
+
+	var slider = $(this);
+
+	slider.slick({
 		// options
+		mobileFirst: true,
+		prevArrow: '<button class="slick-arrow slick-prev"><svg class="sprite sprite--arrow-left">\n' +
+			'\t\t\t<use xlink:href="http://voodookit.loc/custom/themes/voodookit/dist/assets/svg/sprite-symbol.svg#arrow-left"></use>\n' +
+			'\t\t</svg></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><svg class="sprite sprite--arrow-right slick-next">\n' +
+			'\t\t\t<use xlink:href="http://voodookit.loc/custom/themes/voodookit/dist/assets/svg/sprite-symbol.svg#arrow-right"></use>\n' +
+			'\t\t</svg></button>',
+	});
+
+	$('.sprite--arrow-right').on('click', function () {
+		slider.slick('slickNext');
 	})
 });
 
